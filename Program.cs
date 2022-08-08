5866d607bae4c3653a3c00814b1f823a34b114c7
Ella.koles <ella.koles@yandex.ru>
2022-08-08 12:06:30 +0500

    Added homework

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿/* using System;
+class HelloWorld {
+    static void Main() {
+        int num, num2;
+        Console.WriteLine("Введите первое число: ");
+        num = Convert.ToInt32( Console.ReadLine() );
+        Console.WriteLine("Введите второе число: ");
+        num2 = Convert.ToInt32( Console.ReadLine() );
+        if(num > num2) {
+            Console.WriteLine ("max: "+num);
+        }
+            else {
+                Console.WriteLine("max: "+num2);
+            }
+        }
+    }
+
+
+
+using System;
+class HelloWorld {
+    int num;
+    Console.WriteLine("Введите число N: ");
+    num = Convert.ToInt32( Console.ReadLine() );
+
+    int i = 0;
+
+    Console.WriteLine("четные числа :");
+    for (i = 1 <= num; i++);
+    {
+        if ( i % 2 == 0)
+    }
+        Console.Write (i + " ");
+        }
+    }
+    
+    
+        Console.WriteLine ();
+        }
+    }
+
+    */
+
+    int num1, num2, num3;
+
+    Console.Write("введите первое число :");
+    num1 = Convert.ToInt32(Console.ReadLine());
+    Console.Write(введите второй число :);
+    num2 = ConvertToInt32(ConsoleReadLike());
+    ConsoleWrite(введите третье число :);
+    num2 = ConvertToInt32(ConsoleReadLine());
+
+    if (num1 > num2)
+    {
+        if (num1 > num3)
+        {
+            ConsoleWrite("первое чилcо наибольшее" \n\n);
+    else 
+            
